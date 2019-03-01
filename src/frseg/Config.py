d3adb691a797f0e31c9509250210847336c18764
Narinder Singh <ng04111@uga.edu>
2019-03-01 12:19:43 -0500

    Add Config.py module with constants for data file paths and grayscale values

---
+++ src/frseg/Config.py
@@ -0,0 +1,9 @@
+"""
+			Author: Narinder Singh				Project: Cilia Segmentation			Date: 01 Mar 2019
+			Course: CSCI 8360 @ UGA				Semester: Spring 2019				Module: Config.py
+
+Description: This module contains universal configuration for modules.
+"""
+
+DATA_FILES_PATH = "../../files/"
+CILIA_GRAYSCALE = 2
