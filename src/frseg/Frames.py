b517b312193fad6d5ff503533e1e0e93b8851e51
Narinder Singh <ng04111@uga.edu>
2019-03-01 19:13:45 -0500

    Change data type for loading images from int64 to int 32

--- src/frseg/Frames.py
+++ src/frseg/Frames.py
@@ -10,7 +10,8 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as matplot
 
-import Utilities
+from Utilities import *
+from Config import *
 
 class FramesError(Exception): pass
 class BadPathError(FramesError): pass
@@ -33,12 +34,12 @@ def computeMean(dir):
 	
 	# Read each image and add to temp
 	for fname in os.listdir(dir):
-		if not Utilities.isImageFile(fname): continue
+		if not isImageFile(fname): continue
 		fpath = os.path.join(dir, fname)
 	
 		# Read image as a numpy matrix
 		img = Image.open(fpath)
-		mat = np.asarray(img, np.int64)
+		mat = np.asarray(img, np.int32)
 
 		# Add to mean computation
 		if temp is None:
@@ -76,6 +77,7 @@ def computeVariance(dir):
 	
 	# Read each image and compound sum squared distances
 	for fname in os.listdir(dir):
+		if not isImageFile(fname): continue
 		fpath = os.path.join(dir, fname)
 
 		# Read image as a numpy matrix
@@ -89,12 +91,37 @@ def computeVariance(dir):
 
 
 if __name__ == '__main__':
-	dir = "/Users/nsghumman/Documents/DataSciencePracticum/Team-kieffer/files/data/frames/fe397de12453e22d010132fa1927589b41566bdcf6796234e6606836f3c37927"
-	mean = computeMean(dir)
-	var = computeVariance(dir)
+	dir = "/Users/nsghumman/Documents/DataSciencePracticum/Team-kieffer/files/data/frames/"
+	hash = "ad6eac5d0cfc44219b69a507bc987d279568e54af6cb88b3682e26c8c4710970"
+	temp = [ "cf621707b159de8b3e57f31ed68adbc5e239c41b389c0443c98d40d10e886e01",
+			   "dbafcd86679aeada1a8d977d691b89089142cdae380a2f3158099db5db0b713e",
+			   "ad6eac5d0cfc44219b69a507bc987d279568e54af6cb88b3682e26c8c4710970",
+			   "94393237ac4081715d3443b15fadb274bd2095ac9e1992c8ac7e9560631a5f7b",
+			   "77eb15c0e7e51dd9688a5e580b5681a2416fd8a7c82a5a7f6f4d5e2b68aeda0a",
+			   "717f959cad7c9cbf084577ef236e0d39783e43994f1d56705bf1a33aa67d7965",
+			   "7167939da20844cd30f8e63009c73bd89dbb36e3ec38878f32f9781228c53e2b",
+			   "2237443684da4c50851e9c1ebc479859ec30bc52d5ee3a78266c9f07e8f17f91"
+			 ]
+			 
+	means = []
+	imeans = []
+	length = flen(TRAIN_FILE)
+	bar = ProgressBar(length, message = "Processing files .......")
 	
-	matplot.scatter(xrange(len(var.flat)), var.flat)
+	hashes = readLines(TRAIN_FILE)
+	for hash in hashes:
+		fpath = getVideoFramesDirectory(hash)
+		var = computeVariance(fpath)
+		mask = readMask(hash)
+		imask = invertMask(mask)
+		if var[mask].mean() < 40: means.append(var[mask].mean())
+		if var[imask].mean() < 40: imeans.append(var[imask].mean())
+		bar.update()
+
+	matplot.scatter(xrange(len(means)), means, color='red')
+	matplot.scatter(xrange(len(imeans)), imeans, color='blue')
 	matplot.show()
+
 
 
 
